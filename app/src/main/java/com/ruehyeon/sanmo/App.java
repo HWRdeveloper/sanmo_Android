682cc9bb9c950b27aa25af1a3ea0823878c7ee84
HWRdeveloper <54504396+HWRdeveloper@users.noreply.github.com>
2021-07-07 00:57:53 +0900

    commit

--- app/src/main/java/com/ruehyeon/sanmo/App.java
+++ app/src/main/java/com/ruehyeon/sanmo/App.java
@@ -35,6 +35,7 @@ public class App extends Application {
 
                 }
 
+
                 @Override
                 public boolean isUsingWebviewTimer() {
                     return false;
